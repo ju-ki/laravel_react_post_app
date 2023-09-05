e22c2201d611d4b6624701e629fc87c89a49e7d1
ju-ki <juki.programming@gmail.com>
2023-09-05 23:43:13 +0900

    add upvote and downvote model

---
+++ database/migrations/2023_09_04_113540_create_upvotes_downvotes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('upvotes_downvotes', function (Blueprint $table) {
+            $table->id();
+            $table->boolean("is_upvoted");
+            $table->foreignIdFor(Post::class, "post_id");
+            $table->foreignIdFor(User::class, "user_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('upvotes_downvotes');
+    }
+};
